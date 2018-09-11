95d3cd6169c8e184b4e15911e978152fa0bb456e
Furkan Anıl ÖZTAŞ <trpc61@gmail.com>
2018-09-11 19:22:43 +0300

    add Update

--- app/src/main/java/com/example/furka/justweather/MainActivity.java
+++ app/src/main/java/com/example/furka/justweather/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.furka.justweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textCity,textRF,textFeel;
+    EditText editCitySearch;
+    ImageView imageIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textCity = (TextView)findViewById(R.id.textCity);
+        textRF = (TextView)findViewById(R.id.textRF);
+        textFeel = (TextView)findViewById(R.id.textFeel);
+        editCitySearch = (EditText)findViewById(R.id.editCitySearch);
+        imageIcon = (ImageView)findViewById(R.id.imageIcon);
     }
 }
